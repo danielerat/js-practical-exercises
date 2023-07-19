0aa5e1d9fd5afcc17f5ab51a52401cd1e157131e
danielerat <davidodo2015@gmail.com>
2023-07-19 13:35:17 +0200

    Array Statistics fix

--- arrayStatistics.js
+++ arrayStatistics.js
@@ -1,8 +1,8 @@
 function arrayStats(array) {
-  const min = Math.min(array);
-  const max = Math.max(array);
+  const min = Math.min(...array);
+  const max = Math.max(...array);
   const sum = array.reduce((acc, val) => acc + val, 0);
-  const average = sum / array.length;
+  const average = (sum / array.length).toFixed(2);
   return { sum, average, min, max };
 }
 
